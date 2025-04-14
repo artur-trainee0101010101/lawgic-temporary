e9088cc3b0356946fd95b31a1a195a7fb9a881e6
Artur Willonski <artur@willonski.com>
2025-04-14 12:47:39 +0200

    update

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function handleSubmit() {
-  const email = document.getElementById("emailInput")?.value;
-  if (!email) {
-    alert("Please enter your email.");
-    return;
-  }
-  alert(`Thank you! We'll be in touch at ${email}`);
-}
